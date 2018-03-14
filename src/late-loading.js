e3f20d923a0850ea944596985661d6e902294c58
Elizabeth Copeland <elliemcopeland@gmail.com>
2018-03-14 15:30:28 -0400

    Breakpoints for Various screenports
    It's something...?

--- src/late-loading.js
+++ src/late-loading.js
@@ -30,10 +30,37 @@ fetchJsonp('http://munews.wpengine.com/wp-json/wp/v2/posts?categories=8&_embed',
     },
 
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+    //scrollbar: {
+    //  el: '.swiper-scrollbar',
+    //},
+
+    // Default parameters
+    slidesPerView: 4,
+    spaceBetween: 40,
+
+
+    // Responsive breakpoints
+    breakpoints: {
+
+      // when window width is <= 1440px
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+
+      // when window width is <= 1024px
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
+      // when window width is <= 768px
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      }
+    }
+  });
 }).catch(function(ex) {
   console.log('parsing failed', ex);
 });
